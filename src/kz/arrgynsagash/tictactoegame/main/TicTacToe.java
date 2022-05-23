20b75cc295806eb324f3cca7ac19624188d0417d
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-23 16:39:27 +0600

    Initial commit

---
+++ src/kz/arrgynsagash/tictactoegame/main/TicTacToe.java
@@ -0,0 +1,120 @@
+package kz.arrgynsagash.tictactoegame.main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static int fieldSizeY;
+    private static int fieldSizeX;
+    private static char[][] field;
+
+    public static void initField() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeX; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < fieldSizeX; j++)
+                System.out.print(field[i][j] + "|");
+            System.out.println();
+        }
+        for (int i = 0; i <= fieldSizeX * 2 + 1; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    public static void humanTurn() {
+        int x;
+        int y;
+        do {
+            System.out.println("Введите координаты X и Y через пробел >>>");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        }
+        while (!isValidCell(x, y) || isEmptyCell(x, y));
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static boolean isEmptyCell(int x, int y) {
+        return field[y][x] != DOT_EMPTY;
+    }
+
+    private static boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private static boolean checkDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isEmptyCell(x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkWin(char c) {
+        return false;
+    }
+
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        }
+        while (isEmptyCell(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+    public static void main(String[] args) {
+        initField();
+        printField();
+        String answer;
+        do {
+            while (true) {
+                humanTurn();
+                if (checkEndGame(DOT_HUMAN, "Выйграл человек")) break;
+                aiTurn();
+                if (checkEndGame(DOT_AI, "Выйграл компьютер")) break;
+            }
+
+            System.out.println("Хотите сыграть еще? y/n >>> ");
+            answer = SCANNER.next();
+
+        } while (answer.equals("y"));
+        SCANNER.close();
+    }
+
+    public static boolean checkEndGame(char dot, String winMessage) {
+        printField();
+        if (checkWin(dot)) {
+            System.out.println(winMessage);
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("Ничья");
+            return true;
+        }
+        return false;
+    }
+}
